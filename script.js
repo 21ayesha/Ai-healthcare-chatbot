2664ab7472012a8e4f3a2c4a006475b0333fb019
Ayesha Sheikh <sheikhtabasum2001@gmail.com>
2023-12-13 21:24:43 +0530

    modified files

--- script.js
+++ script.js
@@ -8,34 +8,3 @@ login_btn.addEventListener("click", () => {
   form.classList.remove("change-form");
 });
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-  import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyDNiWNuAf564ODrOlg6lNdBTNUxaJgwvjw",
-    authDomain: "healthcare-chat-bot-6d401.firebaseapp.com",
-    projectId: "healthcare-chat-bot-6d401",
-    storageBucket: "healthcare-chat-bot-6d401.appspot.com",
-    messagingSenderId: "847363013728",
-    appId: "1:847363013728:web:2e0c4bbd5743f95e9a1705",
-    measurementId: "G-FMY2XRV007"
-  };
-
-  // Initialize Firebase
-
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
-  //get ref to database services
-
-  const db = getDatabase(app);
-
-  
-</script>
